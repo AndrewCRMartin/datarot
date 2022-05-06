54165a256edcef4c6c6b94b24b9d9a4411c59bdb
Andrew Martin <Andrew C.R. Martin>
2022-05-06 17:40:02 +0100

    Added command line parsing

--- datarot.py
+++ datarot.py
@@ -2,12 +2,20 @@
 import os
 import sys
 import math
+import argparse
 
-SMALL = 0.001
-m = 0.285
-c = -32.477
+#-----------------------------------------------------------------------
+# Defaults
+m        = 0.285
+c        = -32.477
+dataFile = "test/Everything_NR2_SklearnGBReg.csv"
+
+#-----------------------------------------------------------------------
+# Defines
+SMALL    = 0.001 # Angle close enough to zero we don't need to rotate
 
 
+#-----------------------------------------------------------------------
 def FindIntersection(m1, c1, m2, c2):
    """
       Takes the equations for 2 lines and finds the intersection
@@ -36,12 +44,14 @@ def FindIntersection(m1, c1, m2, c2):
    
    return(x, y, True)
 
+#-----------------------------------------------------------------------
 def TranslateDataPoint(x, y, transX, transY):
    x = x + transX
    y = y + transY
    return(x, y)
 
 
+#-----------------------------------------------------------------------
 def TranslateDataArrays(xData, yData, ndata, transX, transY):
    """
       Takes x and y data arrays, the number of items and an x and y
@@ -54,6 +64,7 @@ def TranslateDataArrays(xData, yData, ndata, transX, transY):
    return(xData, yData)
 
 
+#-----------------------------------------------------------------------
 def FindAngle(m1, m2):
    """ 
       Finds the angle between two lines based on their slopes.
@@ -67,12 +78,14 @@ def FindAngle(m1, m2):
    tanTheta = (m1-m2)/(1 + m1*m2)
    return(math.atan(tanTheta))
 
+#-----------------------------------------------------------------------
 def RotateDataPoint(x, y, theta):
    xNew = x * math.cos(theta) - y * math.sin(theta)
    yNew = x * math.sin(theta) + y * math.cos(theta)
    return(xNew, yNew)
    
 
+#-----------------------------------------------------------------------
 def RotateDataArray(xData, yData, nData, theta):
    """
       Takes two arrays of (2D) data points, the number of points and an angle.
@@ -83,24 +96,19 @@ def RotateDataArray(xData, yData, nData, theta):
       (xData[i], yData[i]) = RotateDataPoint(xData[i], yData[i], theta);
    return(xData, yData)
 
-
-
-
-
-
-if(__name__ == '__main__'):
-   # Main program
-   with open ("test/Everything_NR2_SklearnGBReg.csv") as file:
-      ndata = 0
-      xData = []
+#-----------------------------------------------------------------------
+def CorrectAndPrintDataFile(dataFile, m, c):
+   with open (dataFile) as file:
+      ndata     = 0
+      xData     = []
       xDataOrig = []
-      yData = []
+      yData     = []
     
       for buffer in file:
          if(buffer[0] != '#'):
             fields = buffer.split(',')
-            x = float(fields[1])
-            y = float(fields[2])
+            x      = float(fields[1])
+            y      = float(fields[2])
             xData.append(x)
             xDataOrig.append(x)
             yData.append(y)
@@ -115,13 +123,13 @@ if(__name__ == '__main__'):
       if(ok):
          xData, yData = TranslateDataArrays(xData, yData, ndata,
                                             -intersectX, -intersectY)
-         angle = FindAngle(m, 1)
+         angle        = FindAngle(m, 1)
          xData, yData = RotateDataArray(xData, yData, ndata, -angle)
          xData, yData = TranslateDataArrays(xData, yData, ndata,
-                                               intersectX, intersectY)
+                                            intersectX, intersectY)
 
       print("Int: %.3f %.3f" % (intersectX, intersectY))
-      print("Ang: %.3f" % (180.0 * angle / math.pi))
+      print("Ang: %.3f"      % (180.0 * angle / math.pi))
 
       print("Rotated Data:")
       for i in range(ndata):
@@ -130,5 +138,61 @@ if(__name__ == '__main__'):
       print("Rotated Y Data (X original):")
       for i in range(ndata):
          print("%f,%f" % (xDataOrig[i], yData[i]))
+
+#-----------------------------------------------------------------------
+def CorrectDataPoint(x, y, m, c, useOrigX):
+   xOrig = x
+    
+   intersectX, intersectY, ok = FindIntersection(m, c, 1, 0)
+   if(ok):
+      x, y  = TranslateDataPoint(x, y, -intersectX, -intersectY)
+      angle = FindAngle(m, 1)
+      x, y  = RotateDataPoint(x, y, -angle)
+      x, y  = TranslateDataPoint(x, y,  intersectX,  intersectY)
+
+      if(useOrigX):
+         return(xOrig, y)
+
+      return(x, y)
+         
+#-----------------------------------------------------------------------
+def CorrectAndPrintDataPoint(x, y, m, c, useOrigX):
+   x, y = CorrectDataPoint(x, y, m, c, useOrigX)
+   print("%f %f" % (x,y))
+   
+
+#-----------------------------------------------------------------------
+# Main program
+
+if(__name__ == '__main__'):
+   parser = argparse.ArgumentParser()
+   parser.add_argument("-m", "--slope", dest="m", default=m, type=float,
+                       help="Slope of best-fit line")
+   parser.add_argument("-c", "--intercept", dest="c", default=c, type=float,
+                       help="Y intercept of best-fit line")
+   parser.add_argument("dataFileOrX", nargs='?', default=dataFile)
+   parser.add_argument("Y",           nargs='?', default='undef')
+
+   args = parser.parse_args()
+   m    = args.m
+   c    = args.c
+   x    = 0.0
+   y    = 0.0
+
+   if(args.Y == "undef"):
+      dataFile = args.dataFileOrX
+   else:
+      dataFile = ''
+      x        = float(args.dataFileOrX)
+      y        = float(args.Y)
+
+#   print("datafile: " + dataFile)
+#   print("X: " + str(x))
+#   print("Y: " + str(y))
+   
+   if(dataFile == ''):
+      CorrectAndPrintDataPoint(x, y, m, c, True)
+   else:
+      CorrectAndPrintDataFile(dataFile, m, c)
 
 
